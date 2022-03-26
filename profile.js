c8b9df1e77deae6ec39f9e46e14f4df3f7e37080
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-03-26 15:16:57 -0700

    I forgot to save + push everything, whoops

---
+++ profile.js
@@ -0,0 +1,14 @@
+const clickColor = document.getElementById('btn');
+
+const clickPlace = document.getElementById('btn');
+
+const clickRitual = document.getElementById('btn');
+
+function clickColor(evt) {
+    evt.MouseEvent
+}
+
+
+
+
+
